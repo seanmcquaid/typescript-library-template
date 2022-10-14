51d97c4f34d10c54899e5b9b55c83d2c238d167f
seanmcquaid <sean.m.mcquaid@gmail.com>
2022-10-14 12:18:07 +0200

    working on aligning to persist package configs

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 /// <reference types="vitest" />
+/// <reference types="vite/client" />
 
-import { defineConfig } from 'vitest/config';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
   test: {
