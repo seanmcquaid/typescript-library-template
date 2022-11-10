95134da447e367e73230be1c4032aaeb43dba01c
seanmcquaid <sean.m.mcquaid@gmail.com>
2022-11-09 21:53:29 -0500

    cjs

--- rollup.config.js
+++ rollup.config.js
@@ -17,6 +17,7 @@ export default [
         sourcemap: true,
         name: packageJson.name,
         preserveModules: true,
+        exports: 'named',
       },
       {
         dir: packageJson.module,
